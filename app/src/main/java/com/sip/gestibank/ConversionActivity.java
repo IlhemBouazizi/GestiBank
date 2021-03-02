c6bc7745370b422ba9101c64e746ebbb334c29d7
ilhembouazizi <ilhem.bouazizi@gmail.com>
2021-03-02 13:38:00 +0100

    Add spinner in conversion menu

--- app/src/main/java/com/sip/gestibank/ConversionActivity.java
+++ app/src/main/java/com/sip/gestibank/ConversionActivity.java
@@ -2,7 +2,15 @@ package com.sip.gestibank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Spinner;
+
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
 import android.os.Bundle;
+
 
 public class ConversionActivity extends AppCompatActivity {
 
@@ -10,5 +18,29 @@ public class ConversionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion);
+
+        String[] arraySpinner = new String[] {
+                "EUR", "TND", "GBP", "CAD", "CNY"
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinner_devices);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
+
+    public void PerformConversion(View view)
+    {
+        double Coeffesion = 0.8;
+
+        TextView ResultTextView = (TextView)findViewById(R.id.textView_resultat);
+
+        TextView MontantTextView = (TextView)findViewById(R.id.editText_Montant);
+
+        if (!MontantTextView.getText().toString().equals("")) {
+            double MontantValue = Double.parseDouble(MontantTextView.getText().toString());
+
+            ResultTextView.setText(Double.toString(MontantValue * Coeffesion));
+        }
     }
 }
